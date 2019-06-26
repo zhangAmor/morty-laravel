1833d56017c6af5587599602b8d050af81d0ec80
phpzj <phpzjw2016@163.com>
2019-06-26 18:14:03 +0800

    update auto-make-code.php

--- app/Libraries/ThirdParty/AutoMakeCode.class.php
+++ app/Libraries/ThirdParty/AutoMakeCode.class.php
@@ -49,7 +49,7 @@ CLASSEND;
 
         $content = $classHead.$classBody.$classEnd;
 
-        $filename = app_path('Model/'.ucfirst($model_name).'php');
+        $filename = app_path('Model/'.ucfirst($model_name).'.php');
 
         file_put_contents($filename,$content);
 
